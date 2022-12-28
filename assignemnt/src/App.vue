8550ff76bdbbebc6156663650644bdcfe048cba6
Tushar-Mazhar <tushar.jatri@gmail.com>
2022-12-28 23:30:42 +0600

    api implemented

---
+++ assignemnt/src/App.vue
@@ -0,0 +1,57 @@
+<script setup>
+import axios from 'axios'
+import { onBeforeMount,ref } from 'vue';
+
+let userInfo = ref([])
+
+const getUser = async ()=>{
+  await axios.get('http://52.77.211.222:3000/users')
+     .then((user)=>{
+       console.log(user.data)
+       userInfo.value = user.data
+     })
+     .catch(()=>{
+       
+     })
+
+    
+}
+
+
+onBeforeMount(()=>{
+   getUser()
+})
+
+
+
+</script>
+
+<template>
+  <div class="main-div">
+      <div>
+         <input type="text" v-model="search" />
+      </div>
+      <div  v-for="user in userInfo" :key="user.id" class="card"> 
+ 
+          <div > 
+            <img :src="`https://robohash.org/${user.id}?size=300x300`" alt="">
+          </div>
+          <p>{{user.email}}</p>
+          <span>{{user.bio}}</span>
+       </div>
+      
+  </div>
+
+</template>
+
+<style scoped>
+.main-div{
+  display: flex;
+}
+.card{
+  background-color: rgb(76, 113, 113);
+  width: 300px;
+  min-height: 400px;
+  margin: 5px;
+}
+</style>
